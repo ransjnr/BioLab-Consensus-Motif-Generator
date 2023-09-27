ebeb9892befc502da30b2b39958924e464152238
RANSFORD OPPONG <faradayfoundationgh@gmail.com>
2023-09-27 15:44:17 +0000

    frontend and backend update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./styles.css";
+import settingsIcon from "./component/settings-icon.svg"
+import accountIcon from "./component/account-icon.svg"
+
 
 function App() {
   const [motifs, setMotifs] = useState("");
@@ -8,9 +11,9 @@ function App() {
   const analyzeMotifs = async () => {
     try {
       const motifArrayRaw = motifs
-      .replace(/[^A-Za-z0-9,]+/g, "")
-      .split(",")
-      .map((item) => item.trim());
+        .replace(/[^A-Za-z0-9,]+/g, "")
+        .split(",")
+        .map((item) => item.trim());
       const response = await fetch("http://localhost:5000/api/analyze", {
         method: "POST",
         headers: {
@@ -25,52 +28,85 @@ function App() {
       console.error("Error:", error);
     }
   };
+  const resetResults = () => {
+    setResults(null);
+    setMotifs("");
+  };
 
   return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Motifs Analysis</h1>
-      <div className="form-group">
-        <label htmlFor="motifs">Enter Motifs (one per line):</label>
-        <textarea
-          id="motifs"
-          className="form-control"
-          rows="6"
-          value={motifs}
-          onChange={(e) => setMotifs(e.target.value)}
-        ></textarea>
-      </div>
-      <button type="button" className="btn btn-primary" onClick={analyzeMotifs}>
-        Analyze
-      </button>
-
-      {results && (
-        <div className="mt-4">
-          <h2>Results:</h2>
-          <div className="card">
-            <div className="card-body">
-              <p>
-                <strong>Count:</strong>
-              </p>
-              <pre>{JSON.stringify(results.count, null, 2)}</pre>
+    <div>
+      <nav class="navbar">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <h1 class="text-logo custom-logo" >faradBioLab</h1>
+          </a>
+          <ul class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Explore</a>
+          </ul>
+        </div>
+      </nav>
+      <div className="container mt-5">
+        <div className="row">
+          {/* Left Column for Motifs Operations */}
+          <div className="col">
+            <div className="form-group">
+              <label htmlFor="motifs">Enter Motifs (one per line):</label>
+              <textarea
+                id="motifs"
+                className="form-control"
+                rows="6"
+                value={motifs}
+                onChange={(e) => setMotifs(e.target.value)}
+              ></textarea>
             </div>
-          </div>
-          <div className="card mt-3">
-            <div className="card-body">
-              <p>
-                <strong>Profile:</strong>
-              </p>
-              <pre>{JSON.stringify(results.profile, null, 2)}</pre>
+            <div className="form-group d-flex justify-content-between">
+              <button
+                type="button"
+                className="btn btn-primary mt-3"
+                onClick={analyzeMotifs}
+              >
+                Analyze
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-secondary mt-3"
+                onClick={resetResults}
+              >
+                Reset
+              </button>
             </div>
-          </div>
-          <div className="card mt-3">
-            <div className="card-body">
-              <p>
-                <strong>Consensus:</strong> {results.consensus}
-              </p>
-            </div>
+            {results && (
+              <div className="results mt-4">
+                <h2>Results:</h2>
+                <div className="card">
+                  <div className="card-body">
+                    <p>
+                      <strong>Count:</strong>
+                    </p>
+                    <pre>{JSON.stringify(results.count, null, 2)}</pre>
+                  </div>
+                </div>
+                <div className="card mt-3">
+                  <div className="card-body">
+                    <p>
+                      <strong>Profile:</strong>
+                    </p>
+                    <pre>{JSON.stringify(results.profile, null, 2)}</pre>
+                  </div>
+                </div>
+                <div className="card mt-3">
+                  <div className="card-body">
+                    <p>
+                      <strong>Consensus:</strong> {results.consensus}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
